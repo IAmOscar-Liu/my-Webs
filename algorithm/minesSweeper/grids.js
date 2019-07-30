4a77c5a26d7bfe13398f4686b72f0af1d1b3162d
IAmOscar-Liu <44384935+IAmOscar-Liu@users.noreply.github.com>
2019-07-30 16:59:54 +0800

    Update grids.js

--- algorithm/minesSweeper/grids.js
+++ algorithm/minesSweeper/grids.js
@@ -18,21 +18,23 @@ class grids{
     assignMines(indexR,indexC){
         let _count = 0
         let isRepeat = false;
-        while(_count < 10){
+        while(_count < NumOfMines){
             let r = floor(random(NumOfMines))
             let c = floor(random(NumOfMines))
             //this.mines.push(this.cells[r][c])
             if(indexR == r && indexC==c){
                 isRepeat = true;
-                //console.log('find repeat')
-            }
-            for(let j= 0; j<this.mines.length;j++){
-                if(this.mines[j].row == r && this.mines[j].col == c){
-                    isRepeat = true;
-                    //console.log('find repeat')
-                    break;
+                //console.log('find repeat first')
+            }else{
+                for(let j= 0; j<this.mines.length;j++){
+                    if(this.mines[j].row == r && this.mines[j].col == c){
+                        isRepeat = true;
+                        //console.log('find repeat before')
+                        break;
+                    }
                 }
             }
+
             if(!isRepeat){
                 this.mines.push(this.cells[r][c])
                 this.cells[r][c].isMine = true
@@ -76,35 +78,52 @@ class grids{
         if(this.cells[r][c].isMine){
             //console.log('Game over');
             gameStatus = 'gameover';
-        }
-        else if(!this.cells[r][c].visited && this.cells[r][c].numOfMinesNearby!=0){
+        }else if(!this.cells[r][c].visited && this.cells[r][c].numOfMinesNearby!=0){
             this.cells[r][c].visited = true;
             this.cells[r][c].fillColor = [255,0,255]
         }else if(!this.cells[r][c].visited && this.cells[r][c].numOfMinesNearby==0){
             //this.cells[r][c].visited = true;
             //this.cells[r][c].fillColor = [255,0,255]
-            this.stack = []
-            handleStack(this.cells[r][c])
+            this.stack = [];
+            //handleStack(this.cells[r][c]);
+            depthSearch(this.cells[r][c])
         }
     }
 }
 
 
-function handleStack(_current){
-    let current = _current;
-    while(true){
+// function handleStack(_current){
+//     let current = _current;
+//     while(true){
+//         current.visited = true;
+//         current.fillColor = [255,0,255]
+
+//         let next = current.returnRandomNeighbor();
+//         if (next) {
+//             next.visited = true;
+//             Grids.stack.push(current);
+//             current = next;
+//         } else if (Grids.stack.length > 0) {
+//             current = Grids.stack.pop();
+//         } else{
+//             break;
+//         }
+//     }
+// }
+
+function depthSearch(_start){
+    let start = _start;
+    Grids.stack.push(start);
+    while(Grids.stack.length > 0){
+        let current = Grids.stack.pop();
         current.visited = true;
-        current.fillColor = [255,0,255]
-
-        let next = current.returnRandomNeighbor();
-        if (next) {
-            next.visited = true;
-            Grids.stack.push(current);
-            current = next;
-        } else if (Grids.stack.length > 0) {
-            current = Grids.stack.pop();
-        } else{
-            break;
+        current.fillColor = [255,0,255];
+        for(neighbor of current.neighbors){
+            if(current.numOfMinesNearby==0 && !neighbor.visited && !neighbor.isMine){
+                // console.log('push',neighbor.row,neighbor.col);
+                Grids.stack.push(neighbor)
+            }
         }
     }
 }
+
