a6fd4f0a085180b6628c3f6c3d76ed99b9a3a7e9
IAmOscar-Liu <44384935+IAmOscar-Liu@users.noreply.github.com>
2019-07-30 17:02:10 +0800

    Update node.js

--- algorithm/depth search/node.js
+++ algorithm/depth search/node.js
@@ -12,40 +12,4 @@ function Node(value) {
     neighbor.edges.push(this);
   }
 
-  Node.prototype.addStack = function(target,tmp){
-    tmp.push(this)
-    
-    if(this == target){
-      return 'find'
-    }else{
-      let result;
-      let stack=[];
-      for(let i = 0; i < this.edges.length ; i++){
-        stack.push(this.edges[i])
-      }
-      // if(this.parent != null){
-      //   for(let i = 0; i<stack.length; i++){
-      //     if(stack[i] == this.parent){
-      //        stack.splice(i,1)
-      //     }
-      //   }
-      // }
-      while(stack.length>0){
-        let edge = stack.shift();
-        if(!edge.searched){
-          edge.parent = this
-          edge.searched = true
-          result = edge.addStack(target,tmp)
-          if(result == 'find'){
-            break;
-          }
-        }
-      }
-      
-      if(result == 'find'){
-        return 'find'
-      }else{
-        return 'done'
-      }
-    }
-  }
+ 
